732ce7b6bb0ede9f254ddbec50f85833bf6908f0
wen <380198622@qq.com>
2020-07-10 17:41:19 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    hello.word!!
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import './common/stylus/variable'
+#app
+  color: $color-theme 
+
+</style>

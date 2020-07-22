1a2b9d0f9d0649c133d457a5858592769ea6ccee
wen <380198622@qq.com>
2020-07-22 17:51:12 +0800

    播放器基本结构完成

--- src/pages/player/player.vue
+++ src/pages/player/player.vue
@@ -1,23 +1,85 @@
 <template>
     <div class="player" v-show="playlist.length>0">
         <div class="normal-player" v-show="fullScreen">
-            播放器        
+          <div class="background">
+            <img width="100%" height="100%" :src="currentSong?currentSong.image:''">
+          </div>
+          <div class="top">
+            <div class="back" @click="back">
+              <i class="icon-back"></i>
+            </div>
+            <h1 class="title" v-html="currentSong?currentSong.name:' '"></h1>
+            <h2 class="subtitle" v-html="currentSong?currentSong.singer:' '"></h2>
+          </div>
+          <div class="middle">
+            <div class="middle-l">
+              <div class="cd-wrapper">
+                <div class="cd">
+                  <img class="image" :src="currentSong?currentSong.image:''">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="bottom">
+            <div class="operators">
+              <div class="icon i-left">
+                <i class="icon-sequence"></i>
+              </div>
+              <div class="icon i-left">
+                <i class="icon-prev"></i>
+              </div>
+              <div class="icon i-center">
+                <i class="icon-play"></i>
+              </div>
+              <div class="icon i-right">
+                <i class="icon-next"></i>
+              </div>
+              <div class="icon i-right">
+                <i class="icon icon-not-favorite"></i>
+              </div>
+            </div>  
+          </div>   
         </div>
-        <div class="mini-player" v-show="!fullScreen"></div>    
+        <div class="mini-player" v-show="!fullScreen" @click="open">
+          <div class="icon">
+            <img width="40" height="40" :src="currentSong?currentSong.image:''">
+          </div>
+          <div class="text">
+            <h2 class="name" v-html="currentSong?currentSong.name:' '"></h2>
+            <p class="desc" v-html="currentSong?currentSong.singer:' '"></p>
+          </div>
+          <div class="control"></div>
+          <div class="control">
+            <i class="icon-playlist"></i>
+          </div>
+        </div>    
     </div>   
 </template>
 
 <script>
-  import {mapGetters} from 'vuex'
+  import {mapGetters,mapMutations} from 'vuex'
   
   export default {
       name:'player',
       computed: {
           ...mapGetters([
               'fullScreen',
-              'playlist'
+              'playlist',
+              'currentSong'
           ])
       },
+      methods: {
+ 
+        back(){//显示小播放器
+          this.getFullScreen(false)
+        },
+        open(){//显示全屏播放器
+          this.getFullScreen(true)
+        },
+        ...mapMutations([
+          'getFullScreen'
+        ]),
+      }
   }
 </script>
 
